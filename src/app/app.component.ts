d976af7de47aa0faf4518797be1d044b0ebfb71b
Kemorave <Kemorave@gmail.com>
2024-10-22 16:21:51 +0300

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  showVid=false;
+export class AppComponent implements OnInit {
+clik() {
+  if (this.showVid) {
+    this.vidPlaying = true;
+  }else{
+    window.open(this.link, '_blank');
+  }
+}
+  showVid = false;
+  vidPlaying = false;
+  link = 'http://51.15.23.9:8986/buildings';
+
+  ngOnInit(): void {
+    let params = new URL(document.location.toString()).searchParams;
+    this.showVid = params.get('showVid') === 'true';
+  }
+  title() {
+    return this.showVid
+      ? 'تم تدشين مبنى اتمام الذكي'
+      : 'تم تدشين منصة صيانة المرافق وادارة الاصول';
+  }
 }
