3a83992954a73bf893dc95aa7525eacd06850028
kemorave <kemorave@gmail.com>
2024-10-27 23:21:19 +0300

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,18 @@ import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   clik() {
-    this.vidPlaying = true;
+    window.open(this.link, '_blank');
   }
   showVid = false;
   vidPlaying = false;
-  link = 'http://51.15.23.9:8986/buildings';
+  link =
+    'https://app.powerbi.com/view?r=eyJrIjoiMWM1YjQ5NWYtMWQwZi00ZmRhLWFkNmMtZGYyMzJhOTk1NmZiIiwidCI6IjBkOTg0NzYxLTZlODYtNDVjMy1hNjBhLWExYWFlOWY2MjNlZSIsImMiOjl9';
 
   ngOnInit(): void {
     let params = new URL(document.location.toString()).searchParams;
     this.showVid = params.get('showvid') === 'true';
   }
   title() {
-    return 'تم إسقاط مؤشر إستطلاع مشاريع البنية التحتية';
+    return 'تدشين منصة مخيمات حائل الشتوية';
   }
 }
