99c765723cac73edb6cadd13cc785c4d6581e32b
kemorave <kemorave@gmail.com>
2024-10-29 00:59:05 +0300

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'tadsheen';
+export class AppComponent implements OnInit {
+  clik() {
+    this.vidPlaying = true;
+  }
+  showVid = false;
+  vidPlaying = false;
+  link =
+    'https://app.powerbi.com/view?r=eyJrIjoiMWM1YjQ5NWYtMWQwZi00ZmRhLWFkNmMtZGYyMzJhOTk1NmZiIiwidCI6IjBkOTg0NzYxLTZlODYtNDVjMy1hNjBhLWExYWFlOWY2MjNlZSIsImMiOjl9';
+
+  ngOnInit(): void {
+    let params = new URL(document.location.toString()).searchParams;
+    this.showVid = params.get('showvid') === 'true';
+  }
+  title() {
+    return 'تم اطلاق بدء مخيمات حائل الشتوية';
+  }
 }
